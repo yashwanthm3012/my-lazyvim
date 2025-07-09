40969c0c986f60d98f6d3ecc389498a957c97af0
Yashwanth M <dev.yashwanthm3012@gmail.com>
2025-07-09 21:30:02 +0530

    minor changes

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Vague Keymaps
+vim.keymap.set("n", "<leader>sv", function()
+  require("vague").toggle()
+end, { desc = "Toggle Vague" })
